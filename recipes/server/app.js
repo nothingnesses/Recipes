eed5be238e288bbdd08b19d924c45b1c173b7552
nothingnesses <18732253+nothingnesses@users.noreply.github.com>
2024-12-16 13:20:30 +0000

    Set up CORS to allow fronted to make request to backend

--- recipes/server/app.js
+++ recipes/server/app.js
@@ -2,6 +2,11 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const cors = require('cors');
+
+const cors_options = {
+	origin: ['https://recipes-frontend.localhost']
+};
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -13,6 +18,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(cors(cors_options));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
